7fa671c91c61496336d9d080b71edafc86e37c44
KevinRojas1499 <kevinrojas99@gmail.com>
2023-10-17 11:58:51 +0000

    removing rm -rf

--- scripts/eval_mnist.sh
+++ scripts/eval_mnist.sh
@@ -1,7 +1,6 @@
 WORK_DIR="mnist"
 ROOT="./root"
 SAMPLES="eval_fid"
-rm -rf ${ROOT}/${WORK_DIR}_seed_0/eval_fid
 python main.py -cc configs/default_mnist.txt  -sc configs/specific_mnist.txt \
     --mode eval --workdir ${WORK_DIR} --n_gpus_per_node 1 \
     --training_batch_size 64 --testing_batch_size 64 --sampling_batch_size 64 \
--- scripts/eval_swiss_roll.sh
+++ scripts/eval_swiss_roll.sh
@@ -1,8 +1,8 @@
 WORK_DIR="realdsm"
 ROOT="./root"
 SAMPLES="eval_fid"
-rm -rf ${ROOT}/${WORK_DIR}_seed_0/eval_fid
-rm -rf ${ROOT}/trajectory/*.png
+# rm -rf ${ROOT}/${WORK_DIR}_seed_0/eval_fid
+# rm -rf ${ROOT}/trajectory/*.png
 python main.py -cc configs/default_toy_data.txt  --mode eval --workdir ${WORK_DIR} --n_gpus_per_node 1 \
     --training_batch_size 512 --testing_batch_size 512 --sampling_batch_size 512 \
     --dataset multimodal_swissroll --root ${ROOT} \
