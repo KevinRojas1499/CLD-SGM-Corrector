13aeda1de231d77c41da82abee0dcafaee57af65
KevinRojas1499 <kevinrojas99@gmail.com>
2023-10-17 11:49:36 +0000

    Mnist training

--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -1,7 +1,7 @@
-WORK_DIR="mnist-2"
+WORK_DIR="mnist-realdsm"
 ROOT="./root/"
 SAMPLES="eval_fid"
-# rm -rf ${ROOT}/${WORK_DIR}_seed_0
+rm -rf ${ROOT}/${WORK_DIR}_seed_0
 python main.py -cc configs/default_mnist.txt  -sc configs/specific_mnist.txt \
     --mode train --cont_nbr 1 --workdir ${WORK_DIR} --n_gpus_per_node 1 \
     --training_batch_size 64 --testing_batch_size 64 --sampling_batch_size 64 \
